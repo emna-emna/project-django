43d1aaa549fc844d9ceda98e28c56c491400dfef
emna-emna <66226634+emna-emna@users.noreply.github.com>
2020-06-01 01:38:49 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,20 +1,12 @@
-from django.urls import path
+from django.contrib.auth.views import LoginView
+from django.urls import re_path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth import views as auth_views
 
-app_name = 'accounts'
+app_name = 'cart'
 
 urlpatterns = [
-
-    path('register/', views.register_view, name="register_view"),
-    path('login/', LoginView.as_view(), name="login_url"),
-    path('logout_view/', LogoutView.as_view(), name="logout_view"),
-    path('reset_password/', auth_views.PasswordResetView.as_view()),
-    path('profile/', views.view_profile, name='view_profile'),
-    path('profile/edit/', views.edit_profile, name='edit_profile'),
-    path('change-password/', views.change_password, name='change_password'),
-    path('edit_all/', views.edit_all, name='custmize'),
-    path('delete_profile/', views.delete_profile, name='delete_profile'),
-    path('activer_profile/', views.activer_profile, name='activer_profile'),
+    re_path(r'^$', views.cart_detail, name='cart_detail'),
+    re_path(r'^add/(?P<produit_id>\d+)/$', views.cart_add, name='cart_add'),
+    re_path(r'^remove/(?P<produit_id>\d+)/$', views.cart_remove, name='cart_remove'),
+    re_path('login_tow/', LoginView.as_view(), name="login_tow")
 ]
--- views.py
+++ views.py
@@ -1,130 +1,49 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from .forms import CreateUserForm, UserProfile
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-from .decorators import allowed_users
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .models import *
-from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
-from django.contrib.auth import update_session_auth_hash
-from accounts.forms import (
-    EditProfileForm
-)
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.views.decorators.http import require_POST
+from boutique.models import Produit
+from mypfe import settings
+from .cart import Cart
+from cart.forms import CartAddProductForm
+from coupons.forms import CouponApplyForm
+from django.contrib import messages
 
 
 # Create your views here.
 
-
-def register_view(request):
-    form = CreateUserForm()
-    if request.method == 'POST':  # S'il s'agit d'une requête POST
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
-
-            # Added username after video because of error returning customer name if not added
-            Customer.objects.create(
-                user=user,
-                name=user.username,
-            )
-            messages.success(request, 'Un compte a été créé pour ' + username)
-            return redirect("accounts:login_url")
-
-    context = {'form': form}
-    return render(request, 'registration/register.html', context)
+@require_POST
+def cart_add(request, produit_id):
+    cart = Cart(request)
+    produit = get_object_or_404(Produit, id=produit_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(
+            produit=produit,
+            quantity=cd['quantity'],
+            update_quantity=cd['update']
+        )
+        messages.success(request, "Produit ajouté au panier avec succès")
+    return redirect('cart:cart_detail')
 
 
-def login_url(request):
-    if request.method == 'Post':
-        username = request.Post.get('username')
-        password = request.Post.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect(reverse('accounts:view_profile'))
-        else:
-            messages.info(request, 'identifiant ou le mot de passe est incorrect')
-
-    context = {}
-    return render(request, 'registration/login.html', context)
+def cart_remove(request, produit_id):
+    cart = Cart(request)
+    produit = get_object_or_404(Produit, id=produit_id)
+    cart.remove(produit)
+    messages.success(request, "Produit a bien été retiré de votre panier")
+    return redirect('cart:cart_detail')
 
 
-def logout_view(request):
-    logout(request)
-    return redirect('login_url')
-
-
-def view_profile(request, pk=None):
-    if pk:
-        user = User.objects.get(pk=pk)
-    else:
-        user = request.user
-    args = {'user': user}
-    return render(request, 'registration/profile.html', args)
-
-
-def edit_profile(request):
-    if request.method == 'POST':
-        form = EditProfileForm(request.POST, instance=request.user)
-
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('accounts:view_profile'))
-    else:
-        form = EditProfileForm(instance=request.user)
-        args = {'form': form}
-        return render(request, 'registration/edit_profile.html', args)
-
-
-def change_password(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(data=request.POST, user=request.user)
-
-        if form.is_valid():
-            form.save()
-            update_session_auth_hash(request, form.user)
-            return redirect(reverse('accounts:view_profile'))
-        else:
-            return redirect(reverse('accounts:change_password'))
-    else:
-        form = PasswordChangeForm(user=request.user)
-
-        args = {'form': form}
-        return render(request, 'registration/change_password.html', args)
-
-
-def edit_all(request):
-    title = "custmize Profile"
-    try:
-        profile = request.user.userprofile
-    except UserProfile.DoesNotExist:
-        profile = UserProfile(user=request.user)
-
-    if request.method == 'POST':
-        form = UserProfile(request.POST or None, request.FILES or None, instance=profile)
-        if form.is_valid():
-            form.save()
-            return redirect("accounts:view_profile")
-    else:
-        form = UserProfile(instance=userProfile)
-    return render(request, 'registration/editall.html', {'title': title, "form": form})
-
-
-def delete_profile(request):
-    user = request.user
-    user.is_active = False
-    user.save()
-    logout(request)
-    messages.success(request, 'Profile successfully disabled.')
-    return redirect("accounts:login_url")
-
-
-def activer_profile(request):
-    logout(request)
-    user = request.user
-    user.is_active = False
-    user.save()
-    messages.success(request, 'Profile successfully disabled.')
-    return redirect("accounts:view_profile")
+def cart_detail(request):
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
+    coupon_apply_form = CouponApplyForm()
+    context = {
+        'cart': cart,
+        'coupon_apply_form': coupon_apply_form
+    }
+    return render(request, 'cart/detail.html', context)
