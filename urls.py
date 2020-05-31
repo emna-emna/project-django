7b90b4f2da3c95bfcc6ffca75601c13c96a8e63f
emna-emna <66226634+emna-emna@users.noreply.github.com>
2020-06-01 01:35:50 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,13 +1,20 @@
-from django.urls import re_path
+from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views as auth_views
 
-app_name = 'boutique'
+app_name = 'accounts'
 
 urlpatterns = [
 
-    re_path(r'^$', views.product_list, name='product_list'),
-    re_path(r'^(?P<catégorie_libellé>[-\w]+)/$', views.product_list, name='product_list_category'),
-    re_path(r'^(?P<id>\d+)/(?P<libellé>[-\w]+)/$', views.product_detail, name='product_detail'),
-    re_path(r'^search/$', views.search, name='search'),
-
+    path('register/', views.register_view, name="register_view"),
+    path('login/', LoginView.as_view(), name="login_url"),
+    path('logout_view/', LogoutView.as_view(), name="logout_view"),
+    path('reset_password/', auth_views.PasswordResetView.as_view()),
+    path('profile/', views.view_profile, name='view_profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('change-password/', views.change_password, name='change_password'),
+    path('edit_all/', views.edit_all, name='custmize'),
+    path('delete_profile/', views.delete_profile, name='delete_profile'),
+    path('activer_profile/', views.activer_profile, name='activer_profile'),
 ]
--- views.py
+++ views.py
@@ -1,49 +1,130 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Catégorie, Produit
-from cart.forms import CartAddProductForm
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import CreateUserForm, UserProfile
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .decorators import allowed_users
+from django.contrib.auth.decorators import login_required
+from .models import *
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from accounts.forms import (
+    EditProfileForm
+)
 
 
-def product_list(request, catégorie_libellé=None):
-    catégorie = None
-    catégories = Catégorie.objects.all()
-    produits = Produit.objects.filter(disponible=True,)
-
-    if catégorie_libellé:
-        catégorie = get_object_or_404(Catégorie, libellé=catégorie_libellé)
-        produits = produits.filter(catégorie=catégorie)
-    context = {
-        'catégorie': catégorie,
-        'catégories': catégories,
-        'produits': produits
-
-    }
-    return render(request, 'home.html', context)
+# Create your views here.
 
 
-def product_detail(request, id, libellé):
-    produit = get_object_or_404(Produit, id=id, libellé=libellé, disponible=True)
-    cart_product_form = CartAddProductForm()
-    context = {
+def register_view(request):
+    form = CreateUserForm()
+    if request.method == 'POST':  # S'il s'agit d'une requête POST
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
 
-        'produit': produit,
-        'cart_product_form': cart_product_form
-    }
+            # Added username after video because of error returning customer name if not added
+            Customer.objects.create(
+                user=user,
+                name=user.username,
+            )
+            messages.success(request, 'Un compte a été créé pour ' + username)
+            return redirect("accounts:login_url")
 
-    return render(request, 'produit.html', context)
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
 
 
-def search(request,):
-    query = request.GET.get('query')
-    if not query:
-        produits = Produit.objects.all()
+def login_url(request):
+    if request.method == 'Post':
+        username = request.Post.get('username')
+        password = request.Post.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse('accounts:view_profile'))
+        else:
+            messages.info(request, 'identifiant ou le mot de passe est incorrect')
+
+    context = {}
+    return render(request, 'registration/login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login_url')
+
+
+def view_profile(request, pk=None):
+    if pk:
+        user = User.objects.get(pk=pk)
     else:
-        # title contains the query is and query is not sensitive to case.
-        produits = Produit.objects.filter(nom__icontains=query)
-    if not produits.exists():
-        produits = produits.filter(nom=query)
-    nom = "Résultats pour la requête %s"%query
-    context = {
-        'produits': produits,
-        'nom': nom
-    }
-    return render(request, 'search.html', context)
+        user = request.user
+    args = {'user': user}
+    return render(request, 'registration/profile.html', args)
+
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('accounts:view_profile'))
+    else:
+        form = EditProfileForm(instance=request.user)
+        args = {'form': form}
+        return render(request, 'registration/edit_profile.html', args)
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect(reverse('accounts:view_profile'))
+        else:
+            return redirect(reverse('accounts:change_password'))
+    else:
+        form = PasswordChangeForm(user=request.user)
+
+        args = {'form': form}
+        return render(request, 'registration/change_password.html', args)
+
+
+def edit_all(request):
+    title = "custmize Profile"
+    try:
+        profile = request.user.userprofile
+    except UserProfile.DoesNotExist:
+        profile = UserProfile(user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfile(request.POST or None, request.FILES or None, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:view_profile")
+    else:
+        form = UserProfile(instance=userProfile)
+    return render(request, 'registration/editall.html', {'title': title, "form": form})
+
+
+def delete_profile(request):
+    user = request.user
+    user.is_active = False
+    user.save()
+    logout(request)
+    messages.success(request, 'Profile successfully disabled.')
+    return redirect("accounts:login_url")
+
+
+def activer_profile(request):
+    logout(request)
+    user = request.user
+    user.is_active = False
+    user.save()
+    messages.success(request, 'Profile successfully disabled.')
+    return redirect("accounts:view_profile")
