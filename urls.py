68bb8bd158ae41fe1e7ee6264d5273d63dedd254
emna-emna <66226634+emna-emna@users.noreply.github.com>
2020-06-01 01:40:53 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,12 +1,9 @@
-from django.contrib.auth.views import LoginView
 from django.urls import re_path
 from . import views
 
-app_name = 'cart'
+app_name = 'commandes'
 
 urlpatterns = [
-    re_path(r'^$', views.cart_detail, name='cart_detail'),
-    re_path(r'^add/(?P<produit_id>\d+)/$', views.cart_add, name='cart_add'),
-    re_path(r'^remove/(?P<produit_id>\d+)/$', views.cart_remove, name='cart_remove'),
-    re_path('login_tow/', LoginView.as_view(), name="login_tow")
+    re_path(r'^create/$', views.order_create, name='order_create'),
+
 ]
--- views.py
+++ views.py
@@ -1,49 +1,61 @@
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
-from django.views.decorators.http import require_POST
-from boutique.models import Produit
-from mypfe import settings
-from .cart import Cart
-from cart.forms import CartAddProductForm
-from coupons.forms import CouponApplyForm
-from django.contrib import messages
+
+from accounts.decorators import allowed_users
+from .forms import OrderCreateForm
+from .models import CommandeItem, Commande
+from cart.cart import Cart
+from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+"""@allowed_users(allowed_roles=['customer'])"""
 
 
-# Create your views here.
-
-@require_POST
-def cart_add(request, produit_id):
+def order_create(request):
     cart = Cart(request)
-    produit = get_object_or_404(Produit, id=produit_id)
-    form = CartAddProductForm(request.POST)
-    if form.is_valid():
-        cd = form.cleaned_data
-        cart.add(
-            produit=produit,
-            quantity=cd['quantity'],
-            update_quantity=cd['update']
-        )
-        messages.success(request, "Produit ajouté au panier avec succès")
-    return redirect('cart:cart_detail')
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            commande = form.save(commit=False)
+            if cart.coupon:
+                commande.coupon = cart.coupon
+                commande.discount = cart.coupon.discount
+            commande.save()
+            for item in cart:
+                CommandeItem.objects.create(
+                    commande=commande,
+                    produit=item['produit'],
+                    prix=item['prix'],
+                    quantity=item['quantity'])
 
+            cart.clear()
+            context = {
+                'commande': commande,
+            }
+            request.session['commande_id'] = commande.id
 
-def cart_remove(request, produit_id):
-    cart = Cart(request)
-    produit = get_object_or_404(Produit, id=produit_id)
-    cart.remove(produit)
-    messages.success(request, "Produit a bien été retiré de votre panier")
-    return redirect('cart:cart_detail')
+            return redirect(reverse('payment:process'))
+            # return render(request, 'order/created.html', context)
 
-
-def cart_detail(request):
-    cart = Cart(request)
-    for item in cart:
-        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
-    coupon_apply_form = CouponApplyForm()
+    else:
+        form = OrderCreateForm()
     context = {
         'cart': cart,
-        'coupon_apply_form': coupon_apply_form
+        'form': form
     }
-    return render(request, 'cart/detail.html', context)
+    return render(request, 'checkout.html', context)
+
+
+"""
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    Order = get_object_or_404(order, id=order_id)
+    html = render_to_string('order/pdf.html', {'order': Order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="order_{}.pdf"'.format(Order.id)
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
+    return response
+"""
