71cd6f532bc05c29a3482b3e79b46377228fc228
emna-emna <66226634+emna-emna@users.noreply.github.com>
2020-06-11 22:53:49 +0200

    Add files via upload

---
+++ boutique/filters.py
@@ -0,0 +1,12 @@
+import django_filters
+from django_filters import CharFilter
+
+from .models import *
+
+class ProductFilter(django_filters.FilterSet):
+
+
+
+	class Meta:
+		model = Produit
+		fields = ['nom' ]
